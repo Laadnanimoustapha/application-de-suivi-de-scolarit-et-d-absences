58f9d801f3dab92328b32f821889a5158a96792c
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2026-04-19 14:44:30 +0100

    feat: initialize database connection and add user authentication function

--- python/database.py
+++ python/database.py
@@ -1,3 +1,13 @@
 import os
+from sqlalchemy import create_engine, text 
 
 DATABASE_KEY = os.getenv("DATABASE")
+
+engine = create_engine(DATABASE_KEY, pool_pre_ping =True , pool_recycle = 300)
+
+def check_users (username,password):
+    with engine.connect() as conn:
+        query = text("SELECT * FROM users WHERE username = :username AND password = :password")
+        result = conn.execute(query,{"username" : username,"password" : password}).fetchone()
+
+        return result is not None 
