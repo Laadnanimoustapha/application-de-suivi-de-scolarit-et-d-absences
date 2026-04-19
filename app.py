3f136e8adf538c067061ba14724161e5eb8ac53a
mustapha laadnani <mustapha.laadnani.03@edu.uiz.ac.ma>
2026-04-19 18:13:43 +0100

    enable CORS middleware for all origins in FastAPI app

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI,HTTPException
 import python.database
 import python.models
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def main_function():
